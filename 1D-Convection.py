1fe391f24685ea581fa7a8131839a6be6fabfd3e
FabioCatalano <catalanofabio.fc@gmail.com>
2024-12-14 23:49:27 +0100

    Update 1D-Convection
    Make step calculation faster with np functions

--- 1D-Convection.py
+++ 1D-Convection.py
@@ -38,23 +38,35 @@ u[int(L1/dx):int(L2/dx + 1)] = 2
 plt.plot(np.linspace(x0, x0 + L_tot, nx), u)
 
 
-#%% Finite difference scheme
+#%% Finite difference scheme non-opt
+
+start_time = time.time()
 
 un = np.ones(nx) #init temp array
 
-for n in range(nt):
-    # u = u - c*dt/dx*np.diff(u)
-    
+for n in range(nt):    
     un = u.copy()
     for i in range(1, nx):
         u[i] = un[i] - c*dt/dx*(un[i] - un[i - 1])
 
 
+print("--- %s seconds ---" % (time.time() - start_time))
+
 plt.plot(np.linspace(x0, x0 + L_tot, nx), u)
 
+#%% Finite diff opt
+
+start_time = time.time()
 
 
+for n in range(nt):
+    u = u - np.concatenate((np.array([0]), c*dt/dx*np.diff(u)))
+    # subtract 0 from the first and c*dt/dx *diff from the others
+    
 
+print("--- %s seconds ---" % (time.time() - start_time))
+
+plt.plot(np.linspace(x0, x0 + L_tot, nx), u)
 
 
 
